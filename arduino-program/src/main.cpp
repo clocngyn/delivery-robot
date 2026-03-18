c1d85c92a8fb99e89d1aedcd3f8b78f43c56a901
Clocngyn <clocngyn@gmail.com>
2026-03-17 20:18:19 -0700

    added ultrasonic sensor activity example

--- arduino-program/src/main.cpp
+++ arduino-program/src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include <HCSR04.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
+ 
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
