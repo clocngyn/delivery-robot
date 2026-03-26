fc37cdd7117f9343eaa0d1795e90262244bf7e59
Clocngyn <clocngyn@gmail.com>
2026-03-25 17:57:25 -0700

    created websocket file and modified platformio file

--- arduino-program/src/main.cpp
+++ arduino-program/src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
 #include <HCSR04.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <LittleFS.h> // failsafe
+#include <ArduinoJson.h>
+
+
+// set up server for the esp32, port80, name it ws
+AsyncWebServer server(80);
+AsyncWebSocket webSocket("/ws");
+
 
 // put function declarations here:
 int myFunction(int, int);
 
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+               void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.println("Browser connected to WebSocket!"); 
+    client->text("{\"status\":\"ok\"}"); // test json message
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  //int result = myFunction(2, 3);
- 
+  Serial.begin(115200);                       // esp32 can read at 115200 bps
+  WiFi.begin("iPhone", "tset2sdkt5q4");         // 
+  while (WiFi.status() != WL_CONNECTED) {     // yield until wifi connected
+    delay(500);
+    Serial.print("."); 
+  }
+  // print when connected
+  String connectMessage = "\nConnected! IP address: " + WiFi.localIP().toString();
+  Serial.println(connectMessage);
+
+  // check littleFS begin
+  if(!LittleFS.begin()) {
+    Serial.println("LittleFS Mount Failed");
+    return;
+  }
+
+  // connect webSocket listerner
+  webSocket.onEvent(onWebSocketEvent);
+
+  // connect websocket object to server
+  server.addHandler(&webSocket);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){ // send html files to client
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+  server.serveStatic("/", LittleFS, "/");  // fail safe functions, checks in memory for requests that exist
+  server.begin();
+  //setup 
+
 }
 
 
 void loop() {
-  
+  webSocket.cleanupClients();
 }
 
 // put function definitions here:
