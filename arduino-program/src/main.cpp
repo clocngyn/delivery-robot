e46ef7cc9fca43a83a766bb1e1f0b8c021e48567
Clocngyn <clocngyn@gmail.com>
2026-04-02 02:10:30 -0700

    added more detailed comments

--- arduino-program/src/main.cpp
+++ arduino-program/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
-#include <HCSR04.h>
+#include <HCSR04.h>                   // ultrasonic sensor
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
-#include <LittleFS.h> // failsafe
-#include <ArduinoJson.h>
+#include <LittleFS.h>                 // fail safe and data management system
+#include <ArduinoJson.h>              
 
 
-// set up server for the esp32, use port 80, name it ws
-AsyncWebServer server(80);
-AsyncWebSocket webSocket("/ws");
+
+AsyncWebServer server(80);          // declare server using port 80
+AsyncWebSocket webSocket("/ws");    // declare an AsyncWebSocket object with the address "/ws"
 
 
 // put function declarations here:
@@ -17,16 +17,16 @@ int myFunction(int, int);
 // WebSocket listener function
 void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
                void *arg, uint8_t *data, size_t len) {
-                
-  if (type == WS_EVT_CONNECT) {     // on connect
+
+  if (type == WS_EVT_CONNECT) {                           // on connect
     Serial.println("Browser connected to WebSocket!"); 
-    client->text("{\"status\":\"ok\"}"); // test json message
+    client->text("{\"status\":\"ok\"}");                  // test json message
   }
 }
 
 void setup() {
   Serial.begin(115200);                       // esp32 can read at 115200 bps
-  WiFi.begin("iPhone", "tset2sdkt5q4");         // 
+  WiFi.begin("iPhone", "tset2sdkt5q4");         
   while (WiFi.status() != WL_CONNECTED) {     // yield until wifi connected
     delay(500);
     Serial.print("."); 
@@ -35,29 +35,30 @@ void setup() {
   String connectMessage = "\nConnected! IP address: " + WiFi.localIP().toString();
   Serial.println(connectMessage);
 
-  // check littleFS begin
+  // begin littleFS
   if(!LittleFS.begin()) {
     Serial.println("LittleFS Mount Failed");
     return;
   }
 
-  // connect webSocket listerner
+  // connect webSocket listener
   webSocket.onEvent(onWebSocketEvent);
 
   // connect websocket object to server
   server.addHandler(&webSocket);
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){ // send html files to client
-    request->send(LittleFS, "/index.html", "text/html");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){  // if someone requests the root page, "/"
+    request->send(LittleFS, "/index.html", "text/html");        // send the html page, (littleFS, htmlpage, filetype)
   });
-  server.serveStatic("/", LittleFS, "/");  // fail safe functions, checks in memory for requests that exist
+
+  server.serveStatic("/", LittleFS, "/");  // fail safe, if indexing file that does not exist, check littleFS
   server.begin();
-  //setup 
+  
 
 }
 
 
 void loop() {
-  webSocket.cleanupClients();
+  webSocket.cleanupClients();     // cleans up disconnected clients
 }
 
 // put function definitions here:
