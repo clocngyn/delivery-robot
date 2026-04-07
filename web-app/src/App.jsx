a9d21e70640de9f012cb9d4a54fbe6b998518a74
Jaideep Deevi <jaideepdeevi@csu.fullerton.edu>
2026-04-06 21:19:03 -0700

    Test

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -4,29 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Deliveries, setDeliveries] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1>
-          
-        </h1>
-      </div>
-      <h1>Delivery Robot</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className = "Home">
+      {/* Intro */}
+      <header className = "header">
+       <h1>Food Delivery Robot</h1>
+       <p>Project created by Pi Class</p>
+      </header>
       
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    </div>
   )
 }
 
