a0cedd82c1c00b44fe1cf33b6124260623b7aba1
Jaideep Deevi <jaideepdeevi@csu.fullerton.edu>
2026-04-07 13:08:18 -0700

    Second test homepage

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -9,11 +9,18 @@ function App() {
   return (
     <div className = "Home">
       {/* Intro */}
-      <header className = "header">
+      <header className = "Intro">
        <h1>Food Delivery Robot</h1>
        <p>Project created by Pi Class</p>
       </header>
       
+      <section className = "About">
+        <h2>About the Project</h2>
+        <p> The Pi class has been working hard all summer to bring to you a food delivery robot.
+          It is designed to naviagate obstacles and traffic to accuratly reach it's destination in a quick
+          manner to give food to it's user.
+        </p>
+      </section>
     
     </div>
   )
