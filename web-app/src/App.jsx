66fba72bbf7d107e214e79178383fc211727ebd2
Jaideep Deevi <jaideepdeevi@csu.fullerton.edu>
2026-04-07 22:19:00 -0700

    fixed routing

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,25 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './Pages/Home'
-import { Designteam } from './Pages/Designteam'
-import { Hardwareteam } from './Pages/Hardwareteam'
-import { Softwareteam } from './Pages/Softwareteam'
+import Home from './Pages/Home'
+import Designteam from './Pages/Designteam'
+import Hardwareteam from './Pages/Hardwareteam'
+import Softwareteam from './Pages/Softwareteam'
 
 function App() {
-
   return (
-  <HashRouter>
-    <Route>
-      <Route path = "/" element = { <Home/>}/>
-      <Route path = "/Desginteam" element = { <Designteam/>}/>
-      <Route path = "/Hardwareteam" element = { <Hardwareteam/>}/>
-      <Route path = "/Softwareteam" element = { <Softwareteam/>}/>
-    </Route>
-  </HashRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Designteam" element={<Designteam />} />
+        <Route path="/Hardwareteam" element={<Hardwareteam />} />
+        <Route path="/Softwareteam" element={<Softwareteam />} />
+      </Routes>
+    </HashRouter>
   )
 }
-
 export default App
